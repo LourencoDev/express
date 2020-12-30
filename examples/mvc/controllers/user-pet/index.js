ff1ecce5bfbac85315e2eaa6499d0d9cecc6a628
LourencoDev <51246899+LourencoDev@users.noreply.github.com>
2020-12-30 18:26:27 -0300

    Update index.js

--- examples/mvc/controllers/user-pet/index.js
+++ examples/mvc/controllers/user-pet/index.js
@@ -7,7 +7,7 @@ var db = require('../../db');
 exports.name = 'pet';
 exports.prefix = '/user/:user_id';
 
-exports.create = function(req, res, next){
+exports.create = (req, res, next) => {
   var id = req.params.user_id;
   var user = db.users[id];
   var body = req.body;
