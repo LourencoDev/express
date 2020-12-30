3b2f61d8d14bf1c24e5fb967d2df3279ae353028
LourencoDev <51246899+LourencoDev@users.noreply.github.com>
2020-12-30 18:28:00 -0300

    Update index.js

--- examples/mvc/controllers/pet/index.js
+++ examples/mvc/controllers/pet/index.js
@@ -6,24 +6,23 @@ var db = require('../../db');
 
 exports.engine = 'ejs';
 
-exports.before = function(req, res, next){
+exports.before = (req, res, next) => {
   var pet = db.pets[req.params.pet_id];
   if (!pet) return next('route');
   req.pet = pet;
   next();
 };
 
-exports.show = function(req, res, next){
+exports.show = (req, res, next) => {
   res.render('show', { pet: req.pet });
 };
 
-exports.edit = function(req, res, next){
+exports.edit = (req, res, next) => {
   res.render('edit', { pet: req.pet });
 };
 
-exports.update = function(req, res, next){
-  var body = req.body;
-  req.pet.name = body.pet.name;
+exports.update = (req, res, next) => {
+  req.pet.name = req.body.pet.name;
   res.message('Information updated!');
   res.redirect('/pet/' + req.pet.id);
 };
